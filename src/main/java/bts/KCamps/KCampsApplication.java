a9173b3b393a074a3924b0efaf3b74ef0ce5ec56
bohdan.tsiupryk <bohdan.tsiupryk@eleks.com>
2020-10-26 23:34:35 +0200

    initial commit

---
+++ src/main/java/bts/KCamps/KCampsApplication.java
@@ -0,0 +1,13 @@
+package bts.KCamps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KCampsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KCampsApplication.class, args);
+	}
+
+}
