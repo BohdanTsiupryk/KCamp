e96c70454663c7dd3360929879df4a8ccf85cfbf
Bohdan Tsiupryk <bohdan.tsiupryk12@gmail.com>
2021-02-22 02:15:02 +0200

    emulate search by questions

--- src/main/java/bts/KCamps/controllers/SearchController.java
+++ src/main/java/bts/KCamps/controllers/SearchController.java
@@ -1,18 +1,24 @@
 package bts.KCamps.controllers;
 
-import bts.KCamps.dto.CurrentLocationDto;
 import bts.KCamps.dto.QuestionnaireDto;
+import bts.KCamps.enums.Childhood;
+import bts.KCamps.enums.Interesting;
+import bts.KCamps.enums.Location;
 import bts.KCamps.model.Camp;
 import bts.KCamps.model.GoogleCampCoordinate;
+import bts.KCamps.service.CampService;
 import bts.KCamps.service.SearchService;
 import bts.KCamps.util.ControllerUtil;
+import bts.KCamps.util.EnumUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Controller
@@ -20,6 +26,7 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class SearchController {
     private final SearchService searchService;
+    private final CampService campService;
 
     @Value("${google.apiKey}")
     private String apiKey;
@@ -53,8 +60,32 @@ public class SearchController {
     }
 
     @PostMapping(value = "/questionnaire")
-    public String handleQuestionnaire(@ModelAttribute QuestionnaireDto questionnaireDto) {
-        System.out.println(questionnaireDto);
-        return "redirect:index";
+    public String handleQuestionnaire(@ModelAttribute QuestionnaireDto questionnaireDto, Model model) {
+        Set<Interesting> interestingSet = questionnaireDto.getInterests() == null ? new HashSet<>() : EnumUtil.getInterests(questionnaireDto.getInterests());
+        Set<Location> locationSet = questionnaireDto.getLocations() == null ? new HashSet<>() : EnumUtil.getLocations(questionnaireDto.getLocations());
+        Set<Childhood> childhoodSet = questionnaireDto.getChildhoods() == null ? new HashSet<>() : EnumUtil.getChildhoods(questionnaireDto.getChildhoods());
+
+        List<Camp> byParameters = searchService.findByParameters(questionnaireDto.getKeyWords());
+        List<Camp> byLocation = searchService.findByLocation(questionnaireDto.getAddress(), questionnaireDto.getMaxDistance());
+        List<Camp> byTags = campService.findByTags(locationSet, interestingSet, childhoodSet);
+
+        byParameters.retainAll(byLocation);
+        byParameters.retainAll(byTags);
+
+        if (byParameters.isEmpty()) {
+            model.addAttribute("empty", true);
+            return "search";
+        }
+
+        List<GoogleCampCoordinate> coords = byParameters.stream()
+                .map(Camp::getCoordinate)
+                .collect(Collectors.toList());
+
+        model.addAttribute("camps", byParameters);
+        model.addAttribute("coords", coords);
+        model.addAttribute("apiKey", apiKey);
+        model.addAttribute("empty", false);
+        ControllerUtil.addTags(model);
+        return "search";
     }
 }
